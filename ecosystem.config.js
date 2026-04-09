5a79cee564dee901a59170dbcea403957f873149
Amr Lotfy <amr.lotfy.othman@gmail.com>
2026-04-09 14:42:15 +0200

    fix: ecosystem.config.js env_file should be string not array

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'et3am-backend',
       script: 'dist/server.js',
       cwd: './backend',
-      env_file: ['./backend/.env.production'],
+      env_file: './backend/.env.production',
     },
   ],
 };
